9e4062ca67172b9e8cd5f6a701dcbe542037ae45
Django Goat <chango.goat@gmail.com>
2025-06-14 19:34:50 +0200

    feat(app): added modal as well as callback

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
+import logging
 import os
-from slack_bolt import App
-from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+import requests
+from slack_bolt import App
+
+# from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"), signing_secret=os.environ.get("SLACK_SIGNING_SECRET"))
+logging.basicConfig(level=logging.DEBUG)
+
 
 @app.message("hello")
 def message_hello(message, say):
@@ -14,17 +20,153 @@ def message_hello(message, say):
                 "accessory": {
                     "type": "button",
                     "text": {"type": "plain_text", "text": "Click Me"},
-                    "action_id": "button_click"
-                }
+                    "action_id": "button_click",
+                },
             }
         ],
-        text=f"Hey there <@{message['user']}>!"
+        text=f"Hey there <@{message['user']}>!",
     )
+
 
 @app.action("button_click")
 def action_button_click(body, ack, say):
     ack()
     say(f"<@{body['user']['id']}> clicked the button")
 
+
+@app.command("/summary")
+def command_summary(body, ack, say, logger, client, command):
+    ack()
+    logger.debug(body)
+    client.views_open(
+        # Pass a valid trigger_id within 3 seconds of receiving it
+        trigger_id=body["trigger_id"],
+        # View payload
+        view={
+            "type": "modal",
+            # View identifier
+            "callback_id": "summary-modal",
+            "private_metadata": command["channel_id"],
+            "submit": {"type": "plain_text", "text": "Submit", "emoji": True},
+            "close": {"type": "plain_text", "text": "Cancel", "emoji": True},
+            "title": {"type": "plain_text", "text": "Deployment Summary", "emoji": True},
+            "blocks": [
+                {
+                    "type": "input",
+                    "block_id": "athena_updated_text_input",
+                    "element": {
+                        "type": "plain_text_input",
+                        "action_id": "athena_updated_text_input-action",
+                        "placeholder": {"type": "plain_text", "text": "KYCCentral Dev"},
+                    },
+                    "label": {"type": "plain_text", "text": "Athena updated", "emoji": True},
+                },
+                {
+                    "type": "input",
+                    "block_id": "from_version_text_input",
+                    "element": {
+                        "type": "plain_text_input",
+                        "action_id": "from_version_text_input-action",
+                        "placeholder": {
+                            "type": "plain_text",
+                            "text": "vX.X.X",
+                        },
+                    },
+                    "label": {"type": "plain_text", "text": "From version", "emoji": True},
+                },
+                {
+                    "type": "input",
+                    "block_id": "to_version_text_input",
+                    "element": {
+                        "type": "plain_text_input",
+                        "action_id": "to_version_text_input-action",
+                        "placeholder": {
+                            "type": "plain_text",
+                            "text": "vX.X.X",
+                        },
+                    },
+                    "label": {"type": "plain_text", "text": "To version", "emoji": True},
+                },
+                {"type": "divider"},
+                {
+                    "type": "input",
+                    "block_id": "migrations_text_input",
+                    "element": {
+                        "type": "rich_text_input",
+                        "action_id": "migrations_text_input-action",
+                        "initial_value": {
+                            "type": "rich_text",
+                            "elements": [
+                                {"type": "rich_text_section", "elements": [{"type": "text", "text": "None"}]}
+                            ],
+                        },
+                    },
+                    "label": {"type": "plain_text", "text": "Migrations", "emoji": True},
+                },
+                {
+                    "type": "input",
+                    "block_id": "pip_installs_text_input",
+                    "element": {
+                        "type": "rich_text_input",
+                        "action_id": "pip_installs_text_input-action",
+                        "initial_value": {
+                            "type": "rich_text",
+                            "elements": [
+                                {"type": "rich_text_section", "elements": [{"type": "text", "text": "None"}]}
+                            ],
+                        },
+                    },
+                    "label": {"type": "plain_text", "text": "Pip installs", "emoji": True},
+                },
+                {"type": "divider"},
+                {
+                    "type": "input",
+                    "block_id": "release_note_file_input",
+                    "label": {"type": "plain_text", "text": "Upload Release Notes"},
+                    "element": {
+                        "type": "file_input",
+                        "action_id": "release_note_file_input_action",
+                        "filetypes": ["pdf"],
+                        "max_files": 1,
+                    },
+                },
+            ],
+        },
+    )
+
+
+@app.view("summary-modal")
+def handle_summary_submission(ack, body, logger, client):
+    ack()
+    logger.debug(body)
+    channel_id = body["view"]["private_metadata"]
+    state_values = body["view"]["state"]["values"]
+    from_ver = state_values["from_version_text_input"]["from_version_text_input-action"]["value"]
+    to_ver = state_values["to_version_text_input"]["to_version_text_input-action"]["value"]
+    migrations = state_values["migrations_text_input"]["migrations_text_input-action"]["rich_text_value"]
+    pip_install = state_values["pip_installs_text_input"]["pip_installs_text_input-action"]["rich_text_value"]
+    release_notes = state_values["release_note_file_input"]["release_note_file_input_action"]["files"][0]
+    logger.debug(f"\n{from_ver}\n {to_ver}\n {migrations}")
+
+    res = requests.get(
+        release_notes["url_private"],
+        headers={"Authorization": f"Bearer {os.environ.get('SLACK_BOT_TOKEN')}"},
+    )
+    file_bytes = res.content
+
+    client.files_upload_v2(
+        channel=channel_id,
+        title=release_notes["name"],
+        filename=release_notes["name"],
+        file=file_bytes,
+    )
+
+    # client.chat_postMessage(
+    #     channel=channel_id,
+    #     text=f"Release notes <{release_notes['permalink']}|{release_notes['name']}>"
+    # )
+
+
 if __name__ == "__main__":
-    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+    app.start(port=int(os.environ.get("PORT", 3000)))
+    # SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
