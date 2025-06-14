92869177e8d32bcaa066d315ec9594adcf24ac04
Django Goat <chango.goat@gmail.com>
2025-06-14 12:27:07 +0200

    feat(boiler plate): initial commit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A Nix-flake-based Python development environment";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { system = "${system}"; };
+    in
+    {
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          venvDir = ".venv";
+          packages =
+            with pkgs;
+            [
+              ruff
+              uv
+              basedpyright
+              python311
+            ]
+            ++ (with pkgs.python311Packages; [
+              venvShellHook
+              # python-lsp-server
+              # python-lsp-ruff
+              # black
+            ]);
+        };
+      };
+    };
+}
+
